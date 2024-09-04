b379f5b602f907ea147a20672364c703d70eb275
guttomarttins <guttomarttins@gmail.com>
2024-09-04 07:05:10 -0300

    add react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
+import { BrowserRouter, Link, NavLink, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Layout from './module/Layout';
 
 function App() {
-  return <Layout/>;
+  return (
+   <BrowserRouter>
+     <nav style={{ width: 200, display: "flex", justifyContent: "space-evenly" }}>
+        <NavLink to="/" end>Dashboard</NavLink>
+        <NavLink to="/list">List</NavLink>
+        <NavLink to="/add">Add</NavLink>
+     </nav>
+     <Routes>
+      <Route path='/' element={<>This is home page</>}></Route>
+      <Route path='/list' element={<>This is list page</>}></Route>
+      <Route path='/add' element={<>This is add page</>}></Route>
+     </Routes>
+     {/* <Layout/> */} 
+   </BrowserRouter>
+  );
 }
 
 export default App;
